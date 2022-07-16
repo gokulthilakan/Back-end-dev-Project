fbcf2037dfb2af2eae6f39311ae447003c8a6655
GOK <107905518+gokulthilakan@users.noreply.github.com>
2022-07-17 02:36:31 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,16 @@
 <?php
-  define('DB_SERVER', 'localhost');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', '');
-  define('DB_NAME', 'php_crud');
-
-  $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-  if ($conn === false) {
-      die("ERROR: Could not connect. " . mysqli_connect_error());
-  }
+// used to connect to the database
+$host = "127.0.0.1";
+$db_name = "Recipe";
+$username = "root";
+$password = "0055";
+ 
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+ 
+// show error
+catch(PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
 ?>
